8df4751ee50a0a8cff9426f3507397e084d19fc9
shivamchawla29june <31385154+shivamchawla29june@users.noreply.github.com>
2020-04-18 17:36:56 +0000

    Sync LeetCode submission - Fibonacci Number (java)

---
+++ problems/fibonacci_number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int N) {
+        if(N==0 || N==1)
+        {
+            return N;
+        }
+        else
+        {
+            return (fib(N-1)+fib(N-2));
+        }        
+    }
+}
