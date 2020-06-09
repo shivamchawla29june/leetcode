c9b7d31f7bf12be11d9d8c47bb153c59c0761643
shivamchawla29june <31385154+shivamchawla29june@users.noreply.github.com>
2020-06-09 11:46:44 +0000

    Sync LeetCode submission - Global and Local Inversions (java)

---
+++ problems/global_and_local_inversions/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        int max=-1;
+        for(int i=0;i<A.length-2;i++)
+        {
+            max=Math.max(max,A[i]);
+            if(max>A[i+2]) return false;
+        }
+        return true;
+    }
+}
